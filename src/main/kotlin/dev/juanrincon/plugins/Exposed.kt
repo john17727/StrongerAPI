a98d5e415a178fb488695f3e7b4e7c70048cd7a6
Juan Rincon <justanotherone17727@gmail.com>
2022-01-05 15:34:07 -0700

    DB Environment Variables

--- src/main/kotlin/dev/juanrincon/plugins/Exposed.kt
+++ src/main/kotlin/dev/juanrincon/plugins/Exposed.kt
@@ -4,6 +4,17 @@ import io.ktor.application.*
 import org.jetbrains.exposed.sql.Database
 
 fun Application.configureExposed() {
-    Database.connect("jdbc:mysql://localhost:3306/stronger", driver = "com.mysql.cj.jdbc.Driver",
-        user = "root", password = "Mustang1772714")
+    val config = environment.config
+    val databasePath = "ktor.database."
+    val type = config.property(databasePath + "type").getString()
+    val host = config.property(databasePath + "host").getString()
+    val port = config.property(databasePath + "port").getString()
+    val db = config.property(databasePath + "db").getString()
+    val driver = config.property(databasePath + "driver").getString()
+    val user = config.property(databasePath + "user").getString()
+    val password = config.property(databasePath + "password").getString()
+
+    val url = "jdbc:$type://$host:$port/$db"
+
+    Database.connect(url, driver = driver, user = user, password = password)
 }
