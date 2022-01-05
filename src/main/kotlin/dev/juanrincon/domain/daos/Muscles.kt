5a1abafb2867082e4863fce35c0010fe17787154
Juan Rincon <justanotherone17727@gmail.com>
2022-01-04 22:59:18 -0700

    Muscle DAO
    Added the muscle DAO and refactored the muscle repository.

--- src/main/kotlin/dev/juanrincon/domain/daos/Muscles.kt
+++ src/main/kotlin/dev/juanrincon/domain/daos/Muscles.kt
@@ -1,8 +1,21 @@
 package dev.juanrincon.domain.daos
 
+import dev.juanrincon.domain.interfaces.Mapper
+import dev.juanrincon.domain.models.Muscle
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IntIdTable
 
 object Muscles: IntIdTable() {
     val name = varchar("name", 25)
     val imageUrl = varchar("imageUrl", 250).nullable()
+}
+
+class MuscleDAO(id: EntityID<Int>): IntEntity(id), Mapper<Muscle> {
+    companion object : IntEntityClass<MuscleDAO>(Muscles)
+    var name by Muscles.name
+    var imageUrl by Muscles.imageUrl
+
+    override fun toModel() = Muscle(id.value, name, imageUrl)
 }
