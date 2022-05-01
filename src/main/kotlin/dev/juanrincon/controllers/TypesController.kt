438b52e2a07223487bbef19933ad1e04166fa6fd
Juan Rincon <justanotherone17727@gmail.com>
2022-05-01 14:00:05 -0600

    Types Controller

--- src/main/kotlin/dev/juanrincon/controllers/TypesController.kt
+++ src/main/kotlin/dev/juanrincon/controllers/TypesController.kt
@@ -1,5 +1,6 @@
 package dev.juanrincon.controllers
 
+import dev.juanrincon.API_VERSION
 import dev.juanrincon.data.services.CategoryService
 import dev.juanrincon.data.services.MuscleService
 import dev.juanrincon.data.state.ServiceResponse
@@ -9,28 +10,38 @@ import dev.juanrincon.domain.models.ApiResponse
 import io.ktor.http.HttpStatusCode.Companion.NotFound
 import io.ktor.http.HttpStatusCode.Companion.OK
 import io.ktor.server.application.*
+import io.ktor.server.locations.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
+const val TYPES = "$API_VERSION/types"
+const val CATEGORIES = "$TYPES/categories"
+const val MUSCLES = "$TYPES/muscles"
+
+@KtorExperimentalLocationsAPI
+@Location(CATEGORIES)
+class CategoriesRoute
+
+@KtorExperimentalLocationsAPI
+@Location(MUSCLES)
+class MusclesRoute
+
+@KtorExperimentalLocationsAPI
 fun Route.typesController(
     categoryService: CategoryService,
     muscleService: MuscleService
 ) {
-    route("/api/types/category") {
-        get {
-            when (val response = categoryService.getAllCategories()) {
-                is Success -> call.respond(response.status, ApiResponse.success(response.data))
-                is Failed -> call.respond(response.status, ApiResponse.fail(response.message))
-            }
+    get<CategoriesRoute> {
+        when (val response = categoryService.getAllCategories()) {
+            is Success -> call.respond(response.status, ApiResponse.success(response.data))
+            is Failed -> call.respond(response.status, ApiResponse.fail(response.message))
         }
     }
 
-    route("/api/types/muscle") {
-        get {
-            when (val response = muscleService.getAllMuscleTypes()) {
-                is Success -> call.respond(response.status, ApiResponse.success(response.data))
-                is Failed -> call.respond(response.status, ApiResponse.fail(response.message))
-            }
+    get<MusclesRoute> {
+        when (val response = muscleService.getAllMuscleTypes()) {
+            is Success -> call.respond(response.status, ApiResponse.success(response.data))
+            is Failed -> call.respond(response.status, ApiResponse.fail(response.message))
         }
     }
 }
