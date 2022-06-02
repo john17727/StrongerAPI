656ae9f4a917d394ec8cce273dd1aa3473995d3c
Juan Rincon <justanotherone17727@gmail.com>
2022-06-01 20:49:45 -0600

    Ktor Version
    Updated ktor version.

--- src/main/kotlin/app/tracktion/data/utilities/Auth.kt
+++ src/main/kotlin/app/tracktion/data/utilities/Auth.kt
@@ -7,7 +7,7 @@ import javax.crypto.spec.SecretKeySpec
 
 
 fun hash(password: String, environment: ApplicationEnvironment): String {
-    val hashKey = hex(environment.config.propertyOrNull("jwt.secret")?.getString()?:"0")
+    val hashKey = hex(environment.config.propertyOrNull("jwt.secret")?.getString() ?: "0")
     val hmacKey = SecretKeySpec(hashKey, "HmacSHA1")
     val hmac = Mac.getInstance("HmacSHA1")
     hmac.init(hmacKey)
